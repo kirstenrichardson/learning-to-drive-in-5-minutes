036d3e27a29934935f44dd052de4ed2edf3e4247
Antonin RAFFIN <antonin.raffin@ensta.org>
2019-06-10 12:34:06 +0200

    Update vae path

--- vae/train.py
+++ vae/train.py
@@ -97,7 +97,7 @@ for epoch in range(args.n_epochs):
         cv2.imshow("Reconstruction", reconstructed_image)
         cv2.waitKey(1)
 
-save_path = "logs/vae-{}".format(args.z_size)
+save_path = "logs/vae-{}.pkl".format(args.z_size)
 os.makedirs(save_path, exist_ok=True)
 print("Saving to {}".format(save_path))
 vae_controller.set_target_params()
